1739b8f40a1c0b220aceca7f544f8fb69f574cb3
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-04 14:29:33 +0700

    Create the project

---
+++ GradientButtons/GradientButtonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GradientButtonsApp.swift
+//  GradientButtons
+//
+//  Created by Danh Tu on 04/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct GradientButtonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
